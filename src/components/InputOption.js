eda678d747a975d41233c37171798eaf4303701a
Costeira <joaocurveira13@gmail.com>
2022-09-01 16:28:26 +0100

    Feed inputs

---
+++ src/components/InputOption.js
@@ -0,0 +1,13 @@
+import "./InputOption.css";
+
+const InputOption = ({Icon, title, color}) => {
+
+    return(
+        <div className="inputOption">
+            <Icon style={{color}}/>
+            <h4>{title}</h4>
+        </div>
+    )
+}
+
+export default InputOption;
---
+++ src/components/Widgets.js
@@ -0,0 +1,12 @@
+import "./Widgets.css"
+const Widgets = () => {
+
+    return(
+        <div className="widgets">
+
+        </div>
+
+    )
+}
+
+export default Widgets;
