36b357a6d4557c534eb466fba6cbadd38645cdf1
Costeira <joaocurveira13@gmail.com>
2022-09-01 16:56:42 +0100

    post section

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -5,9 +5,18 @@ import EventNoteIcon from '@mui/icons-material/EventNote';
 import CalendarViewDayIcon from '@mui/icons-material/CalendarViewDay';
 import "./Feed.css";
 import InputOption from './InputOption';
+import Post from './Post';
+import { useState } from 'react';
 
 const Feed = () => {
 
+    const [posts,setPosts] = useState([]);
+
+    const sendPost=(e) =>{
+        e.preventDefault();
+    }
+
+    
     return(
         <div className="feed">
             <div className="feed__inputContainer">
@@ -15,7 +24,7 @@ const Feed = () => {
                     <CreateIcon/>
                     <form>
                         <input type="text"/>
-                        <button type="submit">Send</button>
+                        <button onClick={sendPost} type="submit">Send</button>
                     </form>
                 </div>
                 <div className='feed__inputOptions'>
@@ -40,6 +49,15 @@ const Feed = () => {
                         color="#7FC15E"
                     />
                 </div>
+            </div>
+
+            <div className='posts'>
+                <Post
+                    name="João Costeira"
+                    description="This is a test"
+                    message="Anim fugiat aliquip enim in minim quis reprehenderit reprehenderit deserunt elit. Voluptate nulla fugiat dolor ea sit sit consequat labore proident cillum magna mollit. Eiusmod enim minim voluptate exercitation anim magna do et cillum ullamco eu ut. Eiusmod eu ullamco ex tempor officia cupidatat aliqua ex nulla."
+
+                />
             </div>
         </div>
     )
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBFCfXEvrrbOzOzpXBdfXXdxJP8uWl1DhY",
+    authDomain: "linkedin-db2b3.firebaseapp.com",
+    projectId: "linkedin-db2b3",
+    storageBucket: "linkedin-db2b3.appspot.com",
+    messagingSenderId: "234310297161",
+    appId: "1:234310297161:web:7339bbd8027c3b029cc198"
+  };
