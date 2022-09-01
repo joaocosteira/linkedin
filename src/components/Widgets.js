29adc874db40e3a9633125a5058ad0085f624a87
Costeira <joaocurveira13@gmail.com>
2022-09-01 21:39:40 +0100

    finished

--- src/components/Widgets.js
+++ src/components/Widgets.js
@@ -1,9 +1,68 @@
 import "./Widgets.css"
+import InfoIcon from '@mui/icons-material/Info';
+import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
+
+
+const NewsArticle = ({ heading,subtitle }) => (
+    
+    <div className="widgets__article">
+        <div className="widgets__articleLeft">
+            <FiberManualRecordIcon />
+        </div>
+        <div className="widgets__articleRight">
+            <h4>{heading}</h4>
+            <p>{subtitle}</p>
+        </div>
+    </div>
+)
+
 const Widgets = () => {
+
+    const articles = [{
+        heading : "React Project",
+        subtitle : "Lots of Readers"
+    },{
+        heading : "Firebase Backend",
+        subtitle : "Supports this Project"
+    },{
+        heading : "Good Morning",
+        subtitle : "Maybe Something..."
+    },{
+        heading : "Sunny Outside",
+        subtitle : "Weather Report"
+    },{
+        heading : "React Project2",
+        subtitle : "Lots of Readers"
+    },{
+        heading : "Firebase Backend2",
+        subtitle : "Supports this Project"
+    },{
+        heading : "Good Morning2",
+        subtitle : "Maybe Something..."
+    },{
+        heading : "Sunny Outside2",
+        subtitle : "Weather Report"
+    },{
+        heading : "React Project3",
+        subtitle : "Lots of Readers"
+    },{
+        heading : "Firebase Backend3",
+        subtitle : "Supports this Project"
+    },{
+        heading : "Good Morning3",
+        subtitle : "Maybe Something..."
+    },{
+        heading : "Sunny Outside3",
+        subtitle : "Weather Report"
+    }]
 
     return(
         <div className="widgets">
-            <h1>Widgets!!!</h1>
+            <div className="widgets__header">
+                <h2>LinkedIn News</h2>
+                <InfoIcon/>
+            </div>
+            {articles.map( article => <NewsArticle key={article.heading} {...article} /> )}
         </div>
 
     )
